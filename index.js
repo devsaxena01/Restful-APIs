6fcc8447ec8e6584f19f20bce69f349e7064250f
Dev Saxena <devsaxena7668@gmail.com>
2025-07-31 19:25:31 +0530

    completed

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const Users = require('./MOCK_DATA.json')
+const fs = require('fs')
 
 const app = express()
 const PORT = 8000
+
+// Middleware - Plugin
+app.use(express.urlencoded({extended:false}))
+
+// Middleware use
+app.use((req , res , next) => {
+    fs.appendFile('log.txt' , `\n ${Date.now()} ${req.ip} ${req.method} ${req.path}` , (err , data) => {
+        next()
+    })
+})
+app.use((req , res , next) => {
+    console.log("hello from middleware 1");
+    next()
+})
+
+app.use((req , res , next) => {
+    console.log("hello from middleware 2");
+    res.end("hey")
+})
 
 // Routes......
 
@@ -32,20 +52,27 @@ app.get('/api/users/:id' , (req , res) => {
 
 // POST /api/users -> Create new user
 app.post('/api/users' , (req , res) => {
-    // TODO: Create new user
-    return res.json({status:pending})
+    const body = req.body;
+    Users.push({body , id:Users.length+1})
+
+    // to add the data in our file MOCK_DATA.json
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(Users) , (err , data) => {
+        return res.send({status:"success" , id:Users.length})
+    })
 })
 
 // PATCH /api/users/1 -> Edit the user with ID 1
 app.patch('/api/users/:id' , (req , res) => {
-    // TODO: Edit the user with id
-    return res.json({status:pending})
+    const body = req.body;
+    console.log(body)
+    return res.json({status:"pending"})
 })
 
 // DELETE /api/users/1 -> Delete the user with ID 1
 app.delete('/api/users/:id' , (req , res) => {
-    // TODO: Delete the user with id
-    return res.json({status:pending})
+    const body = req.body;
+    console.log(body)
+    return res.json({status:"pending"})
 })
 
 app.listen(PORT , (req , res) => {
